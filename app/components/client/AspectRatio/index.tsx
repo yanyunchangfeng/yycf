b522aa6344abecfe6b9cdca38d5e28f4a0240d44
yanyunchangfeng <yanyunchangfeng@gmail.com>
2024-12-19 12:33:37 +0800

    style: blog photo aspectRadio

--- app/components/client/AspectRatio/index.tsx
+++ app/components/client/AspectRatio/index.tsx
@@ -1,10 +1,10 @@
 import Image, { ImageProps } from 'next/image';
 
 import { AspectRatio } from '@/components/ui/aspect-ratio';
-import clsx from 'clsx';
+import { cn } from '@/lib/utils';
 
 export const AspectRatioImage: React.FC<ImageProps> = ({ className, ...restProps }) => {
-  const classes = clsx('w-[300px]', className);
+  const classes = cn('w-[320px]', className);
   return (
     <div className={classes}>
       <AspectRatio ratio={16 / 9}>
