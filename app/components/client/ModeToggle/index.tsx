bca2db738413a4f95effacaef2b8b1745fecf15c
yanyunchangfeng <yanyunchangfeng@gmail.com>
2024-12-21 09:23:15 +0800

    style: update ModeToggle & remove globals extra class

--- app/components/client/ModeToggle/index.tsx
+++ app/components/client/ModeToggle/index.tsx
@@ -3,8 +3,7 @@
 import React from 'react';
 import { Moon, Sun } from 'lucide-react';
 import { useTheme } from 'next-themes';
-
-import { Button } from '@/components/ui/button';
+// import { Button } from '@/components/ui/button';
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -34,12 +33,12 @@ export function ModeToggle() {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <SidebarMenuButton asChild>
-          <Button variant="outline" size="icon">
-            <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-            <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-            <span className="sr-only">Toggle theme</span>
-          </Button>
+        <SidebarMenuButton variant={'outline'} className="justify-center">
+          {/* <Button variant="outline" size="icon"> */}
+          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          <span className="sr-only">Toggle theme</span>
+          {/* </Button> */}
         </SidebarMenuButton>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">{themeItems}</DropdownMenuContent>
