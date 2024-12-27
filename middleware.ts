0a2e136156675c515a851ad70326e6e40f24bbb1
yanyunchangfeng <yanyunchangfeng@gmail.com>
2024-12-27 09:36:59 +0800

    feat: update SideBar menuitem active

--- middleware.ts
+++ middleware.ts
@@ -4,19 +4,5 @@ import { withSupabase, withRedirect, withLogging } from '@/app/middlewares';
 export default chain([withLogging, withRedirect, withSupabase]);
 
 export const config = {
-  matcher: [
-    {
-      source: '/'
-    },
-    {
-      source: '/blog'
-    },
-    {
-      source: '/notes'
-    }
-  ]
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)']
 };
-
-// /notes/:path* 匹配 /notes、/notes/xxx、/notes/xxx/xxx
-// /notes/:path? 匹配 /notes、/notes/xxx
-// /notes/:path+ 匹配 /notes/xxx、/notes/xxx/xxx
