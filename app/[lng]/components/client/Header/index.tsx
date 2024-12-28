ee73668de623bce29ddccd4fabf4af07fe1d8324
yanyunchangfeng <yanyunchangfeng@gmail.com>
2024-12-28 15:15:07 +0800

    fix: blog page & header

--- app/[lng]/components/client/Header/index.tsx
+++ app/[lng]/components/client/Header/index.tsx
@@ -14,20 +14,20 @@ import { SidebarTrigger } from '@/components/ui/sidebar';
 import { usePathname, useParams } from 'next/navigation';
 import React from 'react';
 import { useTranslation } from '@/app/i18n/client';
+import { removeLocalePrefix } from '@/app/utils';
 
 export const Header: React.FC<React.PropsWithChildren & { lng: string }> = ({ children, lng }) => {
   const pathName = usePathname();
+  const pathWithoutLocale = removeLocalePrefix(pathName);
   const params = useParams();
   const { t } = useTranslation(lng, 'basic');
   const breamCrumbData = React.useMemo(() => {
-    if (!pathName) return;
-    const pathParts = pathName.split('/').filter((part) => part);
+    if (!pathWithoutLocale) return;
+    const pathParts = pathWithoutLocale.split('/').filter((part) => part);
     const newBreadcrumbs = pathParts.map((part, index) => {
       const href = '/' + pathParts.slice(0, index + 1).join('/');
       let label: string | string[] = decodeURIComponent(part);
-      if (label === lng) {
-        label = label.charAt(0).toUpperCase() + label.slice(1);
-      }
+
       if (params.id && part === params.id) {
         // 如果是动态路由的参数，替换为实际值
         label = params.id; // 动态替换 id
@@ -36,25 +36,27 @@ export const Header: React.FC<React.PropsWithChildren & { lng: string }> = ({ ch
       if (params.slug && params.slug.includes(part)) {
         label = part; // 替换 slug
       }
-      return { label: t(label), href, isLast: index === pathParts.length - 1 };
+      return { label: label, href: `/${lng}${href}`, isLast: index === pathParts.length - 1 };
     });
-    return [{ label: t('home'), href: '/', isLast: false }, ...newBreadcrumbs];
-  }, [pathName, params, lng]);
 
+    return [{ label: 'home', href: `/${lng}`, isLast: false }, ...newBreadcrumbs];
+  }, [pathWithoutLocale, params, lng, pathName, t]);
+
+  // const BreadItems = React.useMemo(() => { //切换会影响渲染 故注释
   const BreadItems = breamCrumbData?.map((item) => {
     if (breamCrumbData?.length === 1) {
       return (
         <BreadcrumbItem key={item.href}>
-          <BreadcrumbPage suppressHydrationWarning>{item.label}</BreadcrumbPage>
+          <BreadcrumbPage suppressHydrationWarning>{t(item.label)}</BreadcrumbPage>
         </BreadcrumbItem>
       );
     }
-    if (!item.isLast) {
+    if (!item?.isLast) {
       return (
         <React.Fragment key={item.href}>
           <BreadcrumbItem>
             <BreadcrumbLink href={item.href} suppressHydrationWarning>
-              {item.label}
+              {t(item.label)}
             </BreadcrumbLink>
           </BreadcrumbItem>
           <BreadcrumbSeparator />
@@ -63,10 +65,11 @@ export const Header: React.FC<React.PropsWithChildren & { lng: string }> = ({ ch
     }
     return (
       <BreadcrumbItem key={item.href}>
-        <BreadcrumbPage suppressHydrationWarning>{item.label}</BreadcrumbPage>
+        <BreadcrumbPage suppressHydrationWarning>{t(item.label)}</BreadcrumbPage>
       </BreadcrumbItem>
     );
   });
+  // }, [breamCrumbData]);
 
   return (
     <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
