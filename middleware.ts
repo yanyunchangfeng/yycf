e1bd88ebfb06b773623da250347a7449e5009e2f
yanyunchangfeng <yanyunchangfeng@gmail.com>
2025-01-06 21:13:41 +0800

    chore: update ads reg

--- middleware.ts
+++ middleware.ts
@@ -4,5 +4,5 @@ import { withSupabase, withRedirect, withLogging, withLanguage } from '@/app/mid
 export default chain([withLogging, withRedirect, withSupabase, withLanguage]);
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)']
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|ads.txt).*)']
 };
