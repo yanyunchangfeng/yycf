68ce364474a8e3e178cafa8c0b9f2df4baa3e575
yanyunchangfeng <yanyunchangfeng@gmail.com>
2025-01-09 14:55:06 +0800

    feat: add robots & update middleware withLanguage

--- app/[lng]/middlewares/withLanguage.ts
+++ app/[lng]/middlewares/withLanguage.ts
@@ -3,12 +3,14 @@ import acceptLanguage from 'accept-language';
 import siteMetadata from '@/data/siteMetadata';
 
 const { fallbackLanguage, languages } = siteMetadata;
+// const publicFile = /\.(.*)$/;
+// const excludeFile: string[] = [];
 
-function getLocale(req: NextRequest) {
-  let language = acceptLanguage.get(req.headers.get('Accept-Language'));
-  if (!language) language = fallbackLanguage;
-  return language;
-}
+// function getLocale(req: NextRequest) {
+//   let language = acceptLanguage.get(req.headers.get('Accept-Language'));
+//   if (!language) language = fallbackLanguage;
+//   return language;
+// }
 acceptLanguage.languages(languages);
 
 export const withLanguage = (index: number, next: (request: NextRequest) => void) => {
@@ -24,23 +26,30 @@ export const withLanguage = (index: number, next: (request: NextRequest) => void
     if (filtedLanguage.length > 0) {
       if (filtedLanguage[0] === fallbackLanguage) {
         //下面注释的代码有一个bug 因为如果用户改了浏览器的首选语言会导致切换失败
-        // /zh/xxx 重定向到 `/xxx`
-        // const url = pathname.replace(`/${fallbackLanguage}`, '');
-        // return NextResponse.redirect(new URL(url ? url : '/', request.url));
-        return next(request);
+        // /en/xxx 重定向到 `/xxx`
+        const url = pathname.replace(`/${fallbackLanguage}`, '');
+        return NextResponse.redirect(new URL(url ? url : '/', request.url));
       }
       // 其他跳过
       return next(request);
     }
+
+    // 如果是 public 文件，不重定向
+    // if (publicFile.test(pathname) && excludeFile.indexOf(pathname.substr(1)) == -1) return next(request);
+
     // 获取匹配的 locale
-    const locale = getLocale(request);
-    request.nextUrl.pathname = `/${locale}${pathname}`;
+    // const locale = getLocale(request);
+    // request.nextUrl.pathname = `/${locale}${pathname}`;
 
     // 默认语言不重定向
-    if (locale == fallbackLanguage) {
-      return NextResponse.rewrite(request.nextUrl);
-    }
+    // if (locale == fallbackLanguage) {
+    // return NextResponse.rewrite(request.nextUrl);
+    // }
+
+    request.nextUrl.pathname = `/${fallbackLanguage}${pathname}`;
+    return NextResponse.rewrite(request.nextUrl);
+
     // 重定向，如 /products 重定向到 /en/products
-    return Response.redirect(request.nextUrl);
+    // return Response.redirect(request.nextUrl);
   };
 };
